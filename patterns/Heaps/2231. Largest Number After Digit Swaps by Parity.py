c46d574588f8fe1fee9412022bb85dd3071aa4db
Akash <akabhowmick@gmail.com>
2024-12-15 15:10:19 -0500

    solve 2231

---
+++ patterns/Heaps/2231. Largest Number After Digit Swaps by Parity.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/largest-number-after-digit-swaps-by-parity/description/?envType=problem-list-v2&envId=heap-priority-queue&difficulty=EASY&status=TO_DO
+
+
+class Solution:
+    def largestInteger(self, num: int) -> int:
+        even = []
+        odd = []
+        nums = [-int(n) for n in str(num)]
+        for n in nums:
+            if n % 2:
+                heapq.heappush(even, n)
+            else:
+                heapq.heappush(odd, n)
+
+        res = 0
+        for n in nums:
+            if n % 2:
+                x = -heapq.heappop(even)
+            else:
+                x = -heapq.heappop(odd)
+            res = res * 10 + x
+
+        return res
