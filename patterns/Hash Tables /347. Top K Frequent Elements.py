021c1f1c0e38888a07e5d4ef4521dc2eb836c8ea
Akash <akabhowmick@gmail.com>
2025-05-27 10:09:03 -0400

    solve 49.py

--- patterns/Hash Tables /347. Top K Frequent Elements.py
+++ patterns/Hash Tables /347. Top K Frequent Elements.py
@@ -15,6 +15,33 @@ class Solution:
             else:
                 countByNumbers[i] = 1
         sorteddict = dict(sorted(countByNumbers.items(), key=lambda item: item[1]))
+        # countByNumbers.items() gives key-value pairs (number, frequency).
+        # key=lambda item: item[1] sorts by the frequency.
+        # This returns a list of tuples, sorted in ascending order of frequency 
         sortlist = list(sorteddict.keys())
+        # convert the keys into a list
         sortlist = sortlist[::-1]
+        #Reverse it to get descending order:
         return sortlist[0:k]
+      
+      
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         # Step 1: Count frequency
+#         count = {}
+#         for num in nums:
+#             count[num] = count.get(num, 0) + 1
+
+#         # Step 2: Sort by frequency (value) in descending order
+#         sorted_items = sorted(count.items(), key=lambda item: item[1], reverse=True)
+
+#         # Step 3: Extract only the top k keys
+#         return [item[0] for item in sorted_items[:k]]
+
+
+# sorted(iterable, key=..., reverse=...)
+# iterable: list, tuple, or dict items
+
+# key: a function that returns a value to sort by
+
+# reverse=True: sort descending
--- patterns/Hash Tables /49. Group Anagrams.py
+++ patterns/Hash Tables /49. Group Anagrams.py
@@ -1,19 +1,13 @@
 # https://leetcode.com/problems/group-anagrams/description/?envType=problem-list-v2&envId=hash-table
 # Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
-
+from collections import defaultdict
 from typing import List
-
-
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        countByNumbers = {}
-        for i in nums:
-            if i in countByNumbers:
-                countByNumbers[i] += 1
-            else:
-                countByNumbers[i] = 1
-        sorteddict = dict(sorted(countByNumbers.items(), key=lambda item: item[1]))
-        sortlist = list(sorteddict.keys())
-        sortlist = sortlist[::-1]
-        return sortlist[0:k]
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        return list(anagram_map.values())
+        
